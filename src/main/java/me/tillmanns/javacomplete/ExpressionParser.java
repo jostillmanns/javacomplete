e7b292df9de28f407911213f7548270dba5448c4
Joschka Tillmanns <joschka@tillmanns.me>
2014-12-03 17:29:07 +0100

    add parser

---
+++ src/main/java/me/tillmanns/javacomplete/ExpressionParser.java
@@ -0,0 +1,58 @@
+package me.tillmanns.javacomplete;
+
+import org.pmw.tinylog.Logger;
+
+public class ExpressionParser {
+    private ExpressionParser() {}
+
+    public static String removeParenBody(String s) {
+	int open = 0;
+	int close = 0;
+
+	String result = "";
+	for (int i = 0; i < s.length(); i++) {
+	    if (open == close) {
+		open = 0;
+		close = 0;
+	    }
+
+	    if (s.charAt(i) == '(') {
+		open++;
+	    }
+
+	    if (s.charAt(i) == ')') {
+		close++;
+	    }
+
+	    if (close != open) {
+		continue;
+	    }
+
+	    result = result + s.charAt(i);
+	}
+	
+	return result.replaceAll("(\\))", "()");
+    }
+
+    public static String parse(String expression) {
+	for(Regexp p:Regexp.getValues()) {
+
+	    if (!expression.matches(p.getRegexp()))
+	    	continue;
+
+	    if (p.getId() == -1)
+	    	return "";
+
+	    Logger.debug("matches: {0}", p);
+
+	    return expression.replaceAll(p.getRegexp(), String.format("$%s", p.getId()));
+	}
+
+	return expression;
+    }
+
+    public static String lastElement(String expression) {
+	String[] elements = expression.split(",");
+	return elements[elements.length-1].trim();
+    }
+}
---
+++ src/main/java/me/tillmanns/javacomplete/Regexp.java
@@ -0,0 +1,32 @@
+package me.tillmanns.javacomplete;
+
+public enum Regexp {
+    ASSIGN ("(.*)[=<>!]+\\s*(new\\s*)?(\\w*)(\\s*\\(.*\\))?", 3),
+    INSTANTIATE ("\\s*(new)\\s*(\\w*)\\s*(<\\s*\\w*\\s*>)*\\s*(\\(.*\\))", 2),
+    METHOD ("\\s*(\\w*)\\s*(\\(.*\\))", 1),
+    ARR_ELEM ("\\s*(\\w*)\\[\\d*\\]", 1),
+    RETURN ("return\\s*(\\w*)", 1),
+    LIST ("(\\w*,)+(\\w*)",2 ),
+    NEW_STATEMENT ("\\s*(new)\\s*$", -1),
+    THIS_STATEMENT ("^(this\\.)(.*)", 2);
+
+    private final String regexp;
+    private final int id;
+
+    Regexp(String regexp, int id) {
+	this.regexp = regexp;
+	this.id = id;
+    }
+
+    public String getRegexp() {
+	return regexp;
+    }
+
+    public int getId() {
+	return id;
+    }
+
+    public static Regexp[] getValues() {
+	return Regexp.values();
+    }
+}
