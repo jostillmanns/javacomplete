d0d0c812abb23102c96a1e4e91607238b7bd2ac0
Joschka Tillmanns <joschka@tillmanns.me>
2014-12-07 02:22:11 +0100

    add importadder functionality to the main class

--- src/main/java/me/tillmanns/javacomplete/JavaComplete.java
+++ src/main/java/me/tillmanns/javacomplete/JavaComplete.java
@@ -11,20 +11,26 @@ import org.pmw.tinylog.writers.ConsoleWriter;
 import java.lang.StringBuilder;
 
 import java.io.File;
-
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
 import java.io.BufferedReader;
 import java.io.Closeable;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.newsclub.net.unix.AFUNIXServerSocket;
 import java.net.Socket;
 
-import java.net.Socket;
+import javassist.ClassPool;
 
 class JavaComplete {
     private static final String API_COMPLETE = "complete";
     private static final String API_CLEAN_IMPORTS = "cleanimports";
+    private static final String API_ADD_IMPORT = "addimport";
+
+    private static final String path = "JAVACOMPLETEPATH";
+
+    private ImportAdder adder;
 
     public static void main(String[] args) {
 
@@ -34,8 +40,15 @@ class JavaComplete {
 	    .formatPattern("{level}: {message}")
 	    .activate();
 
+	JavaComplete jc = new JavaComplete();
+	jc.initCompleteLoop();
+    }
+
+    public void initCompleteLoop() {
+
 	SocketHandler socketHandler = null;
 	AFUNIXServerSocket server = null;
+	adder = initImportAdder();
 
 	try {
 	    socketHandler = new SocketHandler();
@@ -49,22 +62,41 @@ class JavaComplete {
 		}
 	    }
 
-
 	} catch (Exception e) {
 	    Logger.trace(e);
 	} finally {
 	    close(server);
-	}	
+	}
     }
 
-    private static void acceptRequest(SocketHandler socketHandler, AFUNIXServerSocket server) throws Exception {
+    private static ImportAdder initImportAdder() {
+	ImportAdder adder = null;
+	try {
+	    adder = new ImportAdder();
+	} catch (FileNotFoundException e) {
+	    Logger.debug("unable to find java src file");
+	} catch (IOException e) {
+	    Logger.debug("unable to read java src file");
+	}
+
+	return adder;
+    }
+
+    private void acceptRequest(SocketHandler socketHandler, AFUNIXServerSocket server) throws Exception {
 	Socket socket;
 	Request request;
-	
+	ClassPool pool;
+
+	pool = ClassPool.getDefault();
+	String env = System.getenv(path);
+	if (env != null)
+	    pool.insertClassPath(System.getenv(path));
+
 	socket = server.accept();
 	Logger.info("acceppted new client");
 
 	request = socketHandler.readSocket(socket);
+	Logger.debug(request.getApicall());
 
 	switch(request.getApicall()) {
 	case API_COMPLETE:
@@ -72,10 +104,14 @@ class JavaComplete {
 	    break;
 
 	case API_CLEAN_IMPORTS:
-	    
 	    new ImportCleaner(request, socket);
-	    
 	    break;
+
+	case API_ADD_IMPORT:
+	    if (adder == null)
+		break;
+
+	    adder.writePackage(request, socket);
 
 	default:
 	    break;
