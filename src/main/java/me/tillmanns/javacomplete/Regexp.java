6d8fec99efb61510c09351f4716bee33daeba623
Joschka Tillmanns <joschka@tillmanns.me>
2014-12-09 22:40:31 +0100

    refine redundant keyword regexp

--- src/main/java/me/tillmanns/javacomplete/Regexp.java
+++ src/main/java/me/tillmanns/javacomplete/Regexp.java
@@ -5,7 +5,7 @@ public enum Regexp {
     INSTANTIATE ("\\s*(new)\\s*(\\w*)\\s*(<\\s*\\w*\\s*>)*\\s*(\\(.*\\))", 2),
     METHOD ("\\s*(\\w*)\\s*(\\(.*\\))", 1),
     ARR_ELEM ("\\s*(\\w*)\\[\\d*\\]", 1),
-    REDUNDANT_KEYWORD ("(return|case)\\s*(\\w*)", 2),
+    REDUNDANT_KEYWORD ("(return|case|public|private|protected)\\s*(\\w*)", 2),
     LIST ("(\\w*,)+(\\w*)",2 ),
     NEW_STATEMENT ("\\s*(new)\\s*$", -1),
     THIS_STATEMENT ("^(this\\.)(.*)", 2);
